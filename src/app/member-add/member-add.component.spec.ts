506ae8edb51d3998255715b799101dc9acb509aa
mwalter <mwalter@newinstance.ch>
2020-05-01 19:22:11 +0200

    unit tests for member add component

--- src/app/member-add/member-add.component.spec.ts
+++ src/app/member-add/member-add.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { MemberAddComponent } from './member-add.component';
+import {MemberAddComponent} from './member-add.component';
 
 describe('MemberAddComponent', () => {
   let component: MemberAddComponent;
@@ -8,18 +8,46 @@ describe('MemberAddComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MemberAddComponent ]
+      declarations: [MemberAddComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MemberAddComponent);
     component = fixture.componentInstance;
+    component.sprintLength = 2;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set max to 10 if sprintLength is 2', () => {
+    expect(component.max).toBe(10);
+  });
+
+  it('should raise add event when method addMember() is called with valid availability', () => {
+    spyOn(component.add, 'emit');
+    component.form.controls.availability.setValue(7);
+
+    component.addMember();
+    expect(component.add.emit).toHaveBeenCalled();
+  });
+
+  it('should not raise add event when method addMember() is called with invalid availability', () => {
+    spyOn(component.add, 'emit');
+    component.form.controls.availability.setValue(17);
+
+    component.addMember();
+    expect(component.add.emit).not.toHaveBeenCalled();
+  });
+
+  it('should raise clear event when method clearSprint() is called', () => {
+    spyOn(component.clear, 'emit');
+
+    component.clearSprint();
+    expect(component.clear.emit).toHaveBeenCalled();
   });
 });
