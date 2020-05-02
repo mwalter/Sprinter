81a78acfff93e706eb33813b944ed7a395e9b37c
mwalter <mwalter@newinstance.ch>
2020-05-02 20:30:15 +0200

    unit tests for member-list component

--- src/app/member-list/member-list.component.spec.ts
+++ src/app/member-list/member-list.component.spec.ts
@@ -1,6 +1,5 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { MemberListComponent } from './member-list.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {MemberListComponent} from './member-list.component';
 
 describe('MemberListComponent', () => {
   let component: MemberListComponent;
@@ -8,18 +7,35 @@ describe('MemberListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MemberListComponent ]
+      declarations: [MemberListComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MemberListComponent);
     component = fixture.componentInstance;
+    const members = new Map<string, number>();
+    members.set('123', 8);
+    members.set('456', 9);
+    component.members = members;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should raise remove event when method removeMember() is called', () => {
+    spyOn(component.remove, 'emit');
+
+    component.removeMember('123');
+    expect(component.remove.emit).toHaveBeenCalled();
+  });
+
+  it('should return index of map entries', () => {
+    expect(component.getIndex('123')).toBe(0);
+    expect(component.getIndex('456')).toBe(1);
+  });
+
 });
